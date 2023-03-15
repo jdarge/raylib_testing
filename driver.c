dfe314a9900a2721737d048d522253d1df15b290
Jan D <79925808+dark-r00t@users.noreply.github.com>
2023-03-15 11:34:45 -0400

    Update driver.c

--- driver.c
+++ driver.c
@@ -1,11 +1,17 @@
+#include "foo.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "foo.h"
 
-int main(void) {
-    
-    char *s = bar();
-    printf("%s\n", s);
-    free(s);
-    return 0;
+int main(int argc, char **argv) {
+
+  if (argc >= 2) {
+    printf("Argument 1: %s\n", argv[1]);
+  }
+
+  char *s = bar();
+  printf("%s\n", s);
+
+  free(s);
+
+  return 0;
 }
