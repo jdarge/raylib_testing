cadba73f9ea12eb6c4836cffa90fc884664e3fcf
Jan D <79925808+dark-r00t@users.noreply.github.com>
2023-03-15 11:02:19 -0400

    Update foo.c

--- src/foo.c
+++ src/foo.c
@@ -5,8 +5,8 @@
 
 char *bar() {
 
-  char *s = (char *)malloc(sizeof(char) * 12);
-  strcpy(s, "Hello World\0");
+    char *s = (char *)malloc(sizeof(char) * 12);
+    strcpy(s, "Hello World\0");
 
-  return s;
+    return s;
 }
